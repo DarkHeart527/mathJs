ed86939fffcdf174129cea06cc1414cb503014bf
James <DarkHeart527@users.noreply.github.com>
2017-11-14 13:19:54 -0800

    Create mathJs.js

---
+++ mathJs.js
@@ -0,0 +1,13 @@
+mathJs = {
+ random: function() {
+  return Math.random();
+ },
+ exp: function(num, exp) {
+  var number;
+  for (let i = 0; i < exp; i++) {
+   num = num * exp;
+   number = num;
+  }
+  return number;
+ },
+}
